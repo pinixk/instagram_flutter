e14a70eb1b555cd75a335e10a4b622a2db7fa208
pinixk <bjkim0110@gmail.com>
2024-11-17 08:23:11 +0900

    init

---
+++ lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<BottomNavigationBarItem> btnNavItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.healing), label: ''),
+    BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: ''),
+  ];
+
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.amberAccent,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: btnNavItems,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black87,
+        currentIndex: _selectedIndex,
+        onTap: _onBtnItemClick,
+      ),
+    );
+  }
+
+  void _onBtnItemClick(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
